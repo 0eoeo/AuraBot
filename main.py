5532db43b120a7ac1dae1b95193f7174916aa319
0eoeo <1707.0eoeo@gmail.com>
2025-04-28 23:51:52 +0300

    Новая версия без js

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import discord
 from discord.ext import commands
-# from recognizer_client import RecognizerClient
-# from tts_generator import TTSGenerator
 from audio_recorder import AudioRecorder
+from recognizer_client import RecognizerClient
+from tts_generator import TTSGenerator
 from config import BOT_TOKEN
 
-# Инициализация клиентов для распознавания речи и TTS
-# recognizer = RecognizerClient()
-# tts = TTSGenerator()
+recognizer = RecognizerClient()
+tts = TTSGenerator()
 
-# Инициализация бота с нужными интентами
 intents = discord.Intents.default()
 intents.messages = True
 intents.voice_states = True
@@ -18,8 +16,7 @@ intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Инициализация AudioRecorder с реальными объектами recognizer и tts
-audio_recorder = AudioRecorder(bot, '', '')
+audio_recorder = AudioRecorder(bot, recognizer, tts)
 
 @bot.event
 async def on_ready():
@@ -27,22 +24,17 @@ async def on_ready():
 
 @bot.command()
 async def join(ctx):
-    """Команда для присоединения к голосовому каналу, где находится пользователь."""
-    print('Запуск join...')
-    try:
-        if ctx.author.voice:
-            channel = ctx.author.voice.channel  # Получаем голосовой канал, в котором находится пользователь
-            await audio_recorder.join(ctx, channel)  # Передаем канал в метод join
-        else:
-            await ctx.send("❌ Вы не находитесь в голосовом канале!")
-    except Exception as e:
-        print(e)
+    """Команда для подключения к голосовому каналу пользователя."""
+    print('➡ Пытаемся подключиться...')
+    if ctx.author.voice:
+        channel = ctx.author.voice.channel
+        await audio_recorder.join(ctx, channel)
+    else:
+        await ctx.send("❌ Вы не находитесь в голосовом канале!")
 
 @bot.command()
 async def leave(ctx):
     """Команда для выхода из голосового канала."""
-    print('Запуск leave...')
     await audio_recorder.leave(ctx)
 
-# Запуск бота
 bot.run(BOT_TOKEN)
