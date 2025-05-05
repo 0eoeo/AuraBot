cfcfb377d93ec53390f30cc8f695ec356096bcab
0eoeo <1707.0eoeo@gmail.com>
2025-05-05 15:49:21 +0300

    Добавление текстовых ответов

--- bot.js
+++ bot.js
@@ -4,6 +4,13 @@ require('dotenv').config();
 const joinCommand = require('./js/commands/join');
 const leaveCommand = require('./js/commands/leave');
 const handleTextMessage = require('./js/text/text_handler');
+const {
+  playbackQueue,
+  isPlayingRef,
+  setIsPlaying,
+  playNext,
+  player
+} = require('./js/voice/manager');
 
 const client = new Client({
   intents: [
